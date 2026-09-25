6123860cb3ad0d00d1d86ec184131c0659bdd554
VFS25 <sevoifp@gmail.com>
2026-09-25 14:57:45 -0500

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste your Apps Script web app URL here (it ends in /exec).
 window.INVENTORY_CONFIG = {
-  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyftDl4RDkKx_yzvMXSosSvzMhWPuOodcA_crThpE7GPcge3tvKApEjdSh53xE_KAY/exec',
   APP_NAME: 'Event Inventory'
 };
